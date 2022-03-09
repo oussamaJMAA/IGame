0ce9323fbadb574a10b7bbff47be5a4ade1fe1d7
rayanguedri <63672654+rayanguedri@users.noreply.github.com>
2022-03-09 17:37:27 +0100

    crud

--- Chat/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.chat {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.chat to javafx.fxml;
-    exports com.example.chat;
-}
--- messanger/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.messanger {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.messanger to javafx.fxml;
-    exports com.example.messanger;
-}
